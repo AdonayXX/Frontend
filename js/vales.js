1c50a07092949290a9ebe74f9cd7968615aa1da3
AlvaroD19G <alduartega@est.utn.ac.cr>
2024-06-10 00:16:06 -0600

    Login con API
    Se conectó la API y se cargaron ciertos datos se hicieron los cambios acordados en clase.

--- js/vales.js
+++ js/vales.js
@@ -25,12 +25,70 @@ function showToast(title, message) {
     }
 }
 
-document.getElementById('btn-rechazarSoli').addEventListener('click', function() {
-    showToast('Solicitud Rechazada', 'La solicitud ha sido rechazada correctamente.');
+//Parte de Generar Solicitud de Vales. 
+let acompananteCount = 0;
+
+document.getElementById('addCompanion').addEventListener('click', function () {
+    if (acompananteCount < 5) {
+        acompananteCount++;
+        const acompDiv = document.getElementById('acompanante' + acompananteCount);
+        if (acompDiv) {
+            acompDiv.style.display = 'block';
+        }
+    } else {
+        alert("No se pueden agregar mas de 5 acompañantes");
+    }
 });
-document.getElementById('btn-agregarSoli').addEventListener('click', function() {
-    showToast('Solicitud Agregada', 'La solicitud ha sido agregada correctamente.');
+
+document.getElementById('removeCompanion').addEventListener('click', function () {
+    if (acompananteCount > 0) {
+        const acompDiv = document.getElementById('acompanante' + acompananteCount);
+        if (acompDiv) {
+            acompDiv.style.display = 'none';
+        }
+        acompananteCount--;
+    }
 });
-document.getElementById('btn-devolver').addEventListener('click', function() {
-    window.location.href = 'dataTableRequest.html';
+//funcion para obtener la fecha y hora
+document.addEventListener("DOMContentLoaded", function () {
+    var today = new Date();
+    var formattedDate = today.getDate() + '/' + (today.getMonth() + 1) + '/' + today.getFullYear();
+    document.getElementById('date').textContent = formattedDate;
 });
+
+
+
+
+//Funciones para cargar API
+var url = 'http://localhost:56336/';
+AxiosData();
+function AxiosData() {
+    axios.get(`${url}api/acompanantes`)
+        .then(response => {
+            console.log(response.data);
+            LlenarAcompanante(response.data);
+            LlenarUP(response.data);
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+}
+
+function LlenarAcompanante(data) {
+    let body = '';
+    for (let index = 0; index < data.length; index++) {
+        body += `<option value = ${data[index].id}>${data[index].Nombre}</option>`;
+    }
+    document.getElementById('acompananteNombre1').innerHTML = body;
+    document.getElementById('acompananteNombre2').innerHTML = body;
+    document.getElementById('acompananteNombre3').innerHTML = body;
+    document.getElementById('acompananteNombre4').innerHTML = body;
+    document.getElementById('acompananteNombre5').innerHTML = body;
+}
+function LlenarUP(data) {
+    let body = '';
+    for (let index = 0; index < array.length; index++) {
+        body += `<option value = ${data[index].id}>${data[index].UnidadProgramatica}</option>`;
+    }
+    document.getElementById('Up').innerHTML = body;
+}
