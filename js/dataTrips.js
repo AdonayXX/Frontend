5f58c7e82cba3c33cdf7928fab30bc44cb2dd591
AdonayXX <emdiazdu@est.utn.ac.cr>
2024-06-14 14:45:01 -0600

    feat: Update search and dataTrips
    Se actualizaron las funciones de búsqueda y dataTrips para mejorar la funcionalidad del sistema.

--- js/dataTrips.js
+++ js/dataTrips.js
@@ -1,58 +1,148 @@
-"use strict";
+const apiBaseUrl = 'https://backend-transporteccss.onrender.com/api/unidades'; 
 
-document.getElementById('searchTrips').addEventListener('keyup', function() {
-  let input = document.getElementById('searchTrips').value.toLowerCase();
-  let rows = Array.from(document.getElementById('tableTrips').getElementsByTagName('tr'));
-
-  rows.filter((row, index) => {
-    if (index === 0) return;
-    let cells = Array.from(row.getElementsByTagName('td'));
-    let match = cells.some(cell => cell.innerText.toLowerCase().includes(input));
-    row.style.display = match ? '' : 'none';
-  });
+document.addEventListener('DOMContentLoaded', function() {
+    loadUnidades();
 });
 
-async function getPatient() {
-  try {
-      const API_URL = 'http://localhost:56336/api/viaje/';
-      const response = await axios.get(API_URL);
-      console.log(response.data);
+document.getElementById('unitForm').addEventListener('submit', function(event) {
+    event.preventDefault();
 
-      const viajes = response.data.viaje;
-      const tableBody = document.querySelector('#viajesTableBody');
+    const unitNumber = document.getElementById('unitNumber').value;
+    const capacity = parseInt(document.getElementById('capacity').value, 10);
+    const resourceType = Array.from(document.getElementById('resourceType').selectedOptions).map(option => option.value).join(', ');
+    const initialMileage = parseInt(document.getElementById('initialMileage').value, 10);
+    const currentMileage = parseInt(document.getElementById('currentMileage').value, 10);
+    const status = document.getElementById('status').value;
+    const dekraDate = document.getElementById('dekraDate').value;
+    const maintenanceType = status === 'En Mantenimiento' ? document.getElementById('maintenanceType').value : '';
+    const driver = document.getElementById('assignedDriver').value;
+    const mileage = maintenanceType === 'Kilometraje' ? document.getElementById('maintenanceMileage').value : '';
 
-      tableBody.innerHTML = '';
+    if (initialMileage < 0 || currentMileage < 0 || capacity <= 0 || driver === '') {
+        showToast('Error', 'Los campos no pueden tener valores negativos o vacíos.');
+        return;
+    }
 
-      viajes.forEach(viaje => {
-          const row = `
-              <tr>
-                  <td><input type="checkbox" /></td>
-                  <td>${viaje.Paciente}</td>
-                  <td>${viaje.LugarSalida}</td>
-                  <td>${viaje.LugarDestino}</td>
-                  <td>${viaje.Condicion}</td>
-                  <td>${viaje.HoraCita}</td>
-                  <td>${viaje.fechacita}</td>
-                  <td>${viaje.Ruta}</td>
-                  <td>${viaje.Posicion}</td>
-                  <td>
-                      <button class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#ausenteModal">Ausente</button>
-                  </td>
-              </tr>
-          `;
-          tableBody.innerHTML += row;
-      });
-  } catch (error) {
-      console.error('There has been a problem:', error);
-  }
+    if (currentMileage < initialMileage) {
+        showToast('Error', 'El kilometraje actual no puede ser menor al kilometraje inicial.');
+        return;
+    }
+
+    if (!validateCapacity(capacity, resourceType)) {
+        return;
+    }
+
+    const unidadData = {
+        idTipoUnidad: 1, 
+        idTipoRecurso: 1, 
+        idFrecuenciaCambio: 1, 
+        capacidadTotal: capacity,
+        capacidadCamas: 2,
+        capacidadSillas: 4,
+        kilometrajeInicial: initialMileage,
+        kilometrajeActual: currentMileage,
+        adelanto: 0,
+        idEstado: 1 
+    };
+
+    axios.post(`${apiBaseUrl}/ingresar`, unidadData)
+        .then(response => {
+            console.log('Unidad creada:', response.data);
+            showToast('Registro exitoso', 'El registro se ha realizado exitosamente.');
+            document.getElementById('unitForm').reset();
+            loadUnidades(); 
+        })
+        .catch(error => {
+            console.error('Error al crear la unidad:', error);
+            showToast('Error', 'Error al crear la unidad.');
+        });
+});
+
+document.getElementById('status').addEventListener('change', function() {
+    const maintenanceFields = document.getElementById('maintenanceFields');
+    if (this.value === 'En Mantenimiento') {
+        maintenanceFields.style.display = 'block';
+    } else {
+        maintenanceFields.style.display = 'none';
+    }
+});
+
+document.getElementById('maintenanceType').addEventListener('change', function() {
+    const mileageField = document.getElementById('mileageField');
+    mileageField.style.display = this.value === 'Kilometraje' ? 'block' : 'none';
+});
+
+function validateCapacity(capacity, resourceType) {
+    if ((resourceType.includes('Ambulancia') || resourceType.includes('CruzRoja') || resourceType.includes('Privado')) && capacity > 8) {
+        showToast('Error', 'La capacidad de una ambulancia, CruzRoja o Privada no puede ser mayor a 8.');
+        return false;
+    }
+    if (resourceType.includes('Pickup') && capacity > 5) {
+        showToast('Error', 'La capacidad de una pickup no puede ser mayor a 5.');
+        return false;
+    }
+    if (resourceType.includes('Moto') && capacity > 2) {
+        showToast('Error', 'La capacidad de una moto no puede ser mayor a 2.');
+        return false;
+    }
+    return true;
 }
-getPatient();
 
-// function exportToPDF() {
-// }
+function loadToastTemplate(callback) {
+    fetch('toast-template.html')
+        .then(response => response.text())
+        .then(data => {
+            const toastContainer = document.getElementById('toast-container');
+            if (toastContainer) {
+                toastContainer.innerHTML = data;
+                if (callback) callback();
+            } else {
+                console.error('Toast container not found');
+            }
+        })
+        .catch(error => console.error('Error loading toast template:', error));
+}
 
-// function exportToExcel() {
-// }
+function showToast(title, message) {
+    loadToastTemplate(() => {
+        const toastElement = document.getElementById('common-toast');
+        if (toastElement) {
+            document.getElementById('common-toast-title').innerText = title;
+            document.getElementById('common-toast-body').innerText = message;
+            const toast = new bootstrap.Toast(toastElement);
+            toast.show();
+        } else {
+            console.error('Toast element not found');
+        }
+    });
+}
 
-// function realizarViajes() {
-// }
+function loadUnidades() {
+    axios.get(`${apiBaseUrl}/obtener`)
+        .then(response => {
+            const unidades = response.data.unidades;
+            const tableBody = document.getElementById('unitTableBody');
+            tableBody.innerHTML = ''; 
+
+            unidades.forEach(unidad => {
+                const newRow = document.createElement('tr');
+                newRow.innerHTML = `
+                    <td>${unidad.unitNumber}</td>
+                    <td>${unidad.capacidadTotal}</td>
+                    <td>${unidad.resourceType}</td>
+                    <td>${unidad.kilometrajeInicial}</td>
+                    <td>${unidad.kilometrajeActual}</td>
+                    <td>${unidad.status}</td>
+                    <td>${unidad.dekraDate}</td>
+                    <td>${unidad.maintenanceType}</td>
+                    <td>${unidad.driver}</td>
+                    <td>${unidad.mileage}</td>
+                `;
+                tableBody.appendChild(newRow);
+            });
+        })
+        .catch(error => {
+            console.error('Error al obtener las unidades:', error);
+            showToast('Error', 'Error al obtener las unidades.');
+        });
+}
--- js/units.js
+++ js/units.js
@@ -1,88 +1,151 @@
+"use strict";
 
-    document.getElementById('unitForm').addEventListener('submit', function(event) {
-        event.preventDefault();
+document.addEventListener('DOMContentLoaded', function() {
+    loadUnidades();
+});
 
-        const unitNumber = document.getElementById('unitNumber').value;
-        const capacity = document.getElementById('capacity').value;
-        const resourceType = Array.from(document.getElementById('resourceType').selectedOptions).map(option => option.value).join(', ');
-        const initialMileage = document.getElementById('initialMileage').value;
-        const currentMileage =document.getElementById('currentMileage').value;
-        const status = document.getElementById('status').value;
-        const dekraDate = document.getElementById('dekraDate').value;
-        const maintenanceType = status === 'En Mantenimiento' ? document.getElementById('maintenanceType').value : '';
-        const driver = document.getElementById('assignedDriver').value;
-        const mileage = maintenanceType === 'Kilometraje' ? document.getElementById('maintenanceMileage').value : '';
+document.getElementById('unitForm').addEventListener('submit', function(event) {
+    event.preventDefault();
 
-        if (initialMileage < 0 || currentMileage < 0 || capacity <= 0 || driver === '') {
-            showToast('Error', 'Los campos no pueden tener valores negativos o vacíos.');
-            return;
-        }
+    const unitNumber = document.getElementById('unitNumber').value;
+    const capacity = parseInt(document.getElementById('capacity').value, 10);
+    const resourceType = Array.from(document.getElementById('resourceType').selectedOptions).map(option => option.value).join(', ');
+    const initialMileage = parseInt(document.getElementById('initialMileage').value, 10);
+    const currentMileage = parseInt(document.getElementById('currentMileage').value, 10);
+    const status = document.getElementById('status').value;
+    const dekraDate = document.getElementById('dekraDate').value;
+    const maintenanceType = status === 'En Mantenimiento' ? document.getElementById('maintenanceType').value : '';
+    const driver = document.getElementById('assignedDriver').value;
+    const mileage = maintenanceType === 'Kilometraje' ? document.getElementById('maintenanceMileage').value : '';
 
-        if (currentMileage < initialMileage) {
-            showToast('Error', 'El kilometraje actual no puede ser menor al kilometraje inicial.');
-            return;
-        }
-
-        const tableBody = document.getElementById('unitTableBody');
-        const newRow = document.createElement('tr');
-        newRow.innerHTML = `
-            <td>${unitNumber}</td>
-            <td>${capacity}</td>
-            <td>${resourceType}</td>
-            <td>${initialMileage}</td>
-            <td>${currentMileage}</td>
-            <td>${status}</td>
-            <td>${dekraDate}</td>
-            <td>${maintenanceType}</td>
-            <td>${driver}</td>
-            <td>${mileage}</td>
-        `;
-        tableBody.appendChild(newRow);
-
-        document.getElementById('unitForm').reset();
-        showToast('Registro exitoso', 'El registro se ha realizado exitosamente.');
-    });
-
-    document.getElementById('status').addEventListener('change', function() {
-        const maintenanceFields = document.getElementById('maintenanceFields');
-        if (this.value === 'En Mantenimiento') {
-            maintenanceFields.style.display = 'block';
-        } else {
-            maintenanceFields.style.display = 'none';
-        }
-    });
-
-    document.getElementById('maintenanceType').addEventListener('change', function() {
-        const mileageField = document.getElementById('mileageField');
-        mileageField.style.display = this.value === 'Kilometraje' ? 'block' : 'none';
-    });
-
-    function loadToastTemplate(callback) {
-        fetch('toast-template.html')
-            .then(response => response.text())
-            .then(data => {
-                const toastContainer = document.getElementById('toast-container');
-                if (toastContainer) {
-                    toastContainer.innerHTML = data;
-                    if (callback) callback();
-                } else {
-                    console.error('Toast container not found');
-                }
-            })
-            .catch(error => console.error('Error loading toast template:', error));
+    if (initialMileage < 0 || currentMileage < 0 || capacity <= 0 || driver === '') {
+        showToast('Error', 'Los campos no pueden tener valores negativos o vacíos.');
+        return;
     }
 
-    function showToast(title, message) {
-        loadToastTemplate(() => {
-            const toastElement = document.getElementById('common-toast');
-            if (toastElement) {
-                document.getElementById('common-toast-title').innerText = title;
-                document.getElementById('common-toast-body').innerText = message;
-                const toast = new bootstrap.Toast(toastElement);
-                toast.show();
-            } else {
-                console.error('Toast element not found');
-            }
+    if (currentMileage < initialMileage) {
+        showToast('Error', 'El kilometraje actual no puede ser menor al kilometraje inicial.');
+        return;
+    }
+
+    if (!validateCapacity(capacity, resourceType)) {
+        return;
+    }
+
+    // Datos para enviar a la API
+    const unidadData = {
+        idTipoUnidad: 1, // Asegúrate de obtener este valor de un campo adecuado
+        idTipoRecurso: 1, // Asegúrate de obtener este valor de un campo adecuado
+        idFrecuenciaCambio: 1, // Asegúrate de obtener este valor de un campo adecuado
+        capacidadTotal: capacity,
+        capacidadCamas: 2, // Actualiza según sea necesario
+        capacidadSillas: 4, // Actualiza según sea necesario
+        kilometrajeInicial: initialMileage,
+        kilometrajeActual: currentMileage,
+        adelanto: 0, // Actualiza según sea necesario
+        idEstado: 1 // Asegúrate de obtener este valor de un campo adecuado
+    };
+
+    // Enviar los datos a la API
+    axios.post('https://backend-transporteccss.onrender.com/api/unidades/ingresar', unidadData)
+        .then(response => {
+            console.log('Unidad creada:', response.data);
+            showToast('Registro exitoso', 'El registro se ha realizado exitosamente.');
+            document.getElementById('unitForm').reset();
+            loadUnidades(); // Recargar las unidades después de agregar una nueva
+        })
+        .catch(error => {
+            console.error('Error al crear la unidad:', error);
+            showToast('Error', 'Error al crear la unidad.');
         });
-    }
+});
 
+document.getElementById('status').addEventListener('change', function() {
+    const maintenanceFields = document.getElementById('maintenanceFields');
+    if (this.value === 'En Mantenimiento') {
+        maintenanceFields.style.display = 'block';
+    } else {
+        maintenanceFields.style.display = 'none';
+    }
+});
+
+document.getElementById('maintenanceType').addEventListener('change', function() {
+    const mileageField = document.getElementById('mileageField');
+    mileageField.style.display = this.value === 'Kilometraje' ? 'block' : 'none';
+});
+
+function validateCapacity(capacity, resourceType) {
+    if ((resourceType.includes('Ambulancia') || resourceType.includes('CruzRoja') || resourceType.includes('Privado')) && capacity > 8) {
+        showToast('Error', 'La capacidad de una ambulancia, CruzRoja o Privada no puede ser mayor a 8.');
+        return false;
+    }
+    if (resourceType.includes('Pickup') && capacity > 5) {
+        showToast('Error', 'La capacidad de una pickup no puede ser mayor a 5.');
+        return false;
+    }
+    if (resourceType.includes('Moto') && capacity > 2) {
+        showToast('Error', 'La capacidad de una moto no puede ser mayor a 2.');
+        return false;
+    }
+    return true;
+}
+
+function loadToastTemplate(callback) {
+    fetch('toast-template.html')
+        .then(response => response.text())
+        .then(data => {
+            const toastContainer = document.getElementById('toast-container');
+            if (toastContainer) {
+                toastContainer.innerHTML = data;
+                if (callback) callback();
+            } else {
+                console.error('Toast container not found');
+            }
+        })
+        .catch(error => console.error('Error loading toast template:', error));
+}
+
+function showToast(title, message) {
+    loadToastTemplate(() => {
+        const toastElement = document.getElementById('common-toast');
+        if (toastElement) {
+            document.getElementById('common-toast-title').innerText = title;
+            document.getElementById('common-toast-body').innerText = message;
+            const toast = new bootstrap.Toast(toastElement);
+            toast.show();
+        } else {
+            console.error('Toast element not found');
+        }
+    });
+}
+
+function loadUnidades() {
+    axios.get('https://backend-transporteccss.onrender.com/api/unidades')
+        .then(response => {
+            console.log('Unidades:', response.data);
+            const unidades = response.data.unidades;
+            const tableBody = document.getElementById('unitTableBody');
+            tableBody.innerHTML = ''; // Limpiar la tabla antes de agregar las unidades
+
+            unidades.forEach(unidad => {
+                const newRow = document.createElement('tr');
+                newRow.innerHTML = `
+                    <td>${unidad.unitNumber}</td>
+                    <td>${unidad.capacidadTotal}</td>
+                    <td>${unidad.resourceType}</td>
+                    <td>${unidad.kilometrajeInicial}</td>
+                    <td>${unidad.kilometrajeActual}</td>
+                    <td>${unidad.status}</td>
+                    <td>${unidad.dekraDate}</td>
+                    <td>${unidad.maintenanceType}</td>
+                    <td>${unidad.driver}</td>
+                    <td>${unidad.mileage}</td>
+                `;
+                tableBody.appendChild(newRow);
+            });
+        })
+        .catch(error => {
+            console.error('Error al obtener las unidades:', error);
+            showToast('Error', 'Error al obtener las unidades.');
+        });
+}
