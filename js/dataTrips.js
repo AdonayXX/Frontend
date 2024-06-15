03c7c1ecb23cb1f2747253eb8987836e97689de9
Denzel Herrera García <deherreraga@est.utn.ac.cr>
2024-06-15 15:12:53 -0600

    GET/Viajes
    Modificación del módulo de viajes y la función GET de viajes.

--- js/dataTrips.js
+++ js/dataTrips.js
@@ -14,36 +14,39 @@ document.getElementById('searchTrips').addEventListener('keyup', function() {
 
 async function getPatient() {
   try {
-      const API_URL = 'http://localhost:56336/api/viaje/';
-      const response = await axios.get(API_URL);
-      console.log(response.data);
+    const API_URL = 'https://backend-transporteccss.onrender.com/api/viaje';
+    const response = await axios.get(API_URL);
+    console.log(response.data);
 
-      const viajes = response.data.viaje;
-      const tableBody = document.querySelector('#viajesTableBody');
+    const viajes = response.data.viaje;
+    const tableBody = document.querySelector('#viajesTableBody');
 
-      tableBody.innerHTML = '';
+    tableBody.innerHTML = '';
 
-      viajes.forEach(viaje => {
-          const row = `
-              <tr>
-                  <td><input type="checkbox" /></td>
-                  <td>${viaje.Paciente}</td>
-                  <td>${viaje.LugarSalida}</td>
-                  <td>${viaje.LugarDestino}</td>
-                  <td>${viaje.Condicion}</td>
-                  <td>${viaje.HoraCita}</td>
-                  <td>${viaje.fechacita}</td>
-                  <td>${viaje.Ruta}</td>
-                  <td>${viaje.Posicion}</td>
-                  <td>
-                      <button class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#ausenteModal">Ausente</button>
-                  </td>
-              </tr>
-          `;
-          tableBody.innerHTML += row;
-      });
+    viajes.forEach(viaje => {
+      const fecha = new Date(viaje.FechaCita);
+      const formattedFechaCita = `${fecha.getFullYear()}-${String(fecha.getMonth() + 1).padStart(2, '0')}-${String(fecha.getDate()).padStart(2, '0')}`;
+
+      const row = `
+        <tr>
+          <td><input type="checkbox"></td>
+          <td>${viaje.idPaciente}</td>
+          <td>${viaje.LugarSalida}</td>
+          <td>${viaje.idUbicacionDestino}</td>
+          <td>${viaje.Condicion}</td>
+          <td>${viaje.HoraCita}</td>
+          <td>${formattedFechaCita}</td>
+          <td>${viaje.Traslado}</td>
+          <td>${viaje.Camilla}</td>
+          <td>
+            <button class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#ausenteModal">Ausente</button>
+          </td>
+        </tr>
+      `;
+      tableBody.innerHTML += row;
+    });
   } catch (error) {
-      console.error('There has been a problem:', error);
+    console.error('There has been a problem:', error);
   }
 }
 getPatient();
