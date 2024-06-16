a824ea49a1dcbdcc9acda026b249dd2a01130580
KiaraPicado <kiaramarcelapicadopaniagua@gmail.com>
2024-06-16 13:02:26 -0600

    Cargar datos en la tabla
    Creacion del script request y modificacion del html request

--- js/request.js
+++ js/request.js
@@ -1,13 +1,30 @@
-//Funcion para cargar API
-var url = 'https://backend-transporteccss.onrender.com/';
-AxiosData();
-function AxiosData() {
-    axios.get(`${url}api/acompanantes`)
-        .then(response => {
-            console.log(response.data);
-            LlenarAcompanante(response.data);
-        })
-        .catch(error => {
-            console.error('There was a problem with the fetch operation:', error);
+async function getVales() {
+    try {
+        const response = await axios.get(`${url}api/vales`);
+        const vales = response.data; 
+        console.log("Datos de los vales:", vales);
+        
+        const tableBody = document.querySelector('#tableRequest'); 
+        tableBody.innerHTML = '';
+
+        vales.forEach(vale => {
+            const row = `
+                <tr>
+                    <td>${vale.IdVale}</td>
+                    <td>${vale.Fecha_Solicitud}</td>
+                    <td>${vale.NombreSolicitante}</td>
+                    <td>${vale.Motivo}</td>
+                    <td>${vale.EstadoVale}</td> 
+                    <td class="text-center">
+                        <button onclick="loadContent('formVale.html', 'mainContent')" type="button" class="btn btn-outline-secondary">Coordinar</button>
+                    </td>
+                </tr>
+            `;
+            tableBody.innerHTML += row;
         });
+    } catch (error) {
+        console.error('Hubo un problema con la operación de obtención:', error);
+       
+    }
+    getVales();
 }
