a59165ed5e9985511ae8bdd12297885b54fddd00
KiaraPicado <kiaramarcelapicadopaniagua@gmail.com>
2024-07-19 14:28:21 -0600

    Cambios minimos
    Se eliminan unas cosas de exportar a pdf

--- js/pruebapdf.js
+++ js/pruebapdf.js
@@ -1,15 +1,19 @@
-document.getElementById('saveBtn').addEventListener('click', function() {
-    // Obtiene el contenido del div
-    let content = document.getElementById('content').outerHTML;
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('saveBtn').addEventListener('click', function () {
+        var element = document.querySelector('.mt-3.fade-in'); // Adjust this selector to include all the content you want to export
 
-    // Crea un blob con el contenido
-    let blob = new Blob([content], { type: 'text/html' });
+        if (element) {
+            var opt = {
+                margin: 10,
+                filename: 'viajes.pdf',
+                image: { type: 'jpeg', quality: 1.0 },
+                html2canvas: { scale: 2 },
+                jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
+            };
 
-    // Crea un enlace para la descarga
-    let a = document.createElement('a');
-    a.href = URL.createObjectURL(blob);
-    a.download = 'pagina.html';
-
-    // Simula un click en el enlace para iniciar la descarga
-    a.click();
+            html2pdf().set(opt).from(element).save();
+        } else {
+            console.error('No se encontró el elemento para exportar a PDF');
+        }
+    });
 });
