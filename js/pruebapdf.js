69e3fce83653d6119b886303871568045098a4a3
KiaraPicado <kiaramarcelapicadopaniagua@gmail.com>
2024-07-29 03:11:30 -0600

    Update pruebapdf.js
    Cambio minimo

--- js/pruebapdf.js
+++ js/pruebapdf.js
@@ -137,7 +137,7 @@
 //             console.error("Error al obtener datos desde la API o al generar el PDF:", error)
 //         );
 // });
-// EXPORTAR A LA TABLA DE CITAS
+// EXPORTAR CITAS
 // function exportToPDF() {
 //     const token = localStorage.getItem('token');
 
