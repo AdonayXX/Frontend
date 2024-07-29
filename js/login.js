144eb0df1c53eae3adfa2b90fa64ee2f8a29b97c
DufoX18 <carodri323@gmail.com>
2024-07-29 17:33:17 -0600

    actualización
    se crean dos nuevos módulos del login y además se cambia estética del login

--- js/login.js
+++ js/login.js
@@ -41,6 +41,23 @@ function loadToastTemplate(callback) {
         .catch(error => console.error('Error al cargar la plantilla de toast:', error));
 }
 
+document.getElementById("confirmarCorreo").addEventListener("click", function (event) {
+    event.preventDefault();
+    var email = document.getElementById("emailUsuario").value;
+    var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (emailPattern.test(email)) {
+        showToast("¡Éxito!", "Correo enviado exitosamente.");
+
+        setTimeout(() => {
+            window.location.href = "login.html";
+        }, 3500);
+
+    } else {
+        showToast("Atención", "Ingrese un correo válido.");
+    
+    }
+});
+
 function showToast(title, message, reloadCallback) {
     loadToastTemplate(() => {
         const toastElement = document.getElementById('common-toast');
