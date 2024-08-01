2f54fdf05b7e1d551fb6ab053e4649925a1596b6
DufoX18 <carodri323@gmail.com>
2024-08-01 10:41:46 -0600

    Paginación
    se cambia la paginación de todas las tablas

--- js/completedTrips.js
+++ js/completedTrips.js
@@ -67,6 +67,8 @@ async function loadViajes() {
             ordering: false,
             searching: true,
             paging: true,
+            pageLength: 25, 
+            lengthMenu: [ [25, 50, 100, -1], [25, 50, 100, "Todo"] ],
             language: {
                 url: 'https://cdn.datatables.net/plug-ins/1.10.24/i18n/Spanish.json'
             },
--- js/tableFuelATAP.js
+++ js/tableFuelATAP.js
@@ -58,6 +58,8 @@ async function getRegistrosCombustibleATAP() {
                 ordering: false,
                 searching: true,
                 paging: true,
+                pageLength: 25, 
+                lengthMenu: [ [25, 50, 100, -1], [25, 50, 100, "Todo"] ],
                 language: {
                     url: 'https://cdn.datatables.net/plug-ins/1.10.24/i18n/Spanish.json'
                 },
