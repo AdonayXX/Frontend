f6e7487bb7cf506a394ce6b0061e53679f1c6dc7
Alvarikoki <thealvaro875@gmail.com>
2024-08-03 00:42:17 -0600

    formManager
    Actualizaciones

--- js/formManager.js
+++ js/formManager.js
@@ -2,49 +2,179 @@
     const token = localStorage.getItem('token');
     var url = 'https://backend-transporteccss.onrender.com/';
     const btnAdd = document.getElementById('btnGuardar');
+    const btnRead = document.getElementById('btnBuscar');
+    const btnUpdate = document.getElementById('btnActualizar');
+    const btnClear = document.getElementById('btnClear');
+    const btnDelete = document.getElementById('btnEliminar');
 
     async function addManager() {
-        try {
-            const manager = {
-                Nombre: document.getElementById('nombre').value,
-                Cargo: document.getElementById('cargo').value,
-                Contacto: document.getElementById('email').value,
-            };
-            const response = await axios.post(`${url}api/funcionarios`, manager, {
-                headers: {
-                    'Authorization': `Bearer ${token}`
-                }
-            });
-            showToast('Éxito', 'Encargado guardado con éxito.');
-            return true;
-        } catch (error) {
-            console.error('Error al guardar datos:', error.response ? error.response.data : error.message);
-            showToast('Error', 'Error al guardar el encargado.');
-            return false;
+        ced = document.getElementById('id').value;
+        nombre = document.getElementById('nombre').value;
+        apellidos = document.getElementById('apellidos').value;
+        cargo = document.getElementById('cargo').value;
+        contacto = document.getElementById('contacto').value;
+        email = document.getElementById('email').value;
+
+        if (ced !== null && ced !== "" && nombre !== null && nombre !== "" && apellidos !== null && apellidos !== "" &&
+            cargo !== null && cargo !== "" && contacto !== null && contacto !== "" && email !== null && email !== "") {
+            try {
+                const manager = {
+                    Cedula: ced,
+                    Nombre: nombre,
+                    Apellidos: apellidos,
+                    Cargo: cargo,
+                    Contacto: contacto,
+                    Correo: email,
+                };
+                const response = await axios.post(`${url}api/funcionarios`, manager, {
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                });
+                showToast('Éxito', 'Encargado guardado con éxito.');
+                return true;
+            } catch (error) {
+                console.error('Error al guardar datos:', error.response ? error.response.data : error.message);
+                showToast('Error', 'Error al guardar el encargado.');
+                return false;
+            }
+        } else {
+            showToast('Error', 'Completa la información necesaria');
+        }
+
+    }
+
+    async function readManager() {
+        id = document.getElementById('id').value;
+        if (id) {
+            try {
+                const response = await axios.get(`${url}api/funcionarios`, {
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                });
+                const managers = response.data;
+                managers.forEach(manager => {
+                    if (manager.Cedula == id) {
+                        document.getElementById('nombre').value = manager.Nombre;
+                        document.getElementById('apellidos').value = manager.Apellidos;
+                        document.getElementById('cargo').value = manager.Cargo;
+                        document.getElementById('contacto').value = manager.Contacto;
+                        document.getElementById('email').value = manager.Correo;
+                        return true;
+                    }
+                });
+            } catch (error) {
+                console.error('Error al buscar datos', error.response ? error.response.data : error.message);
+                showToast('Error', 'Error al buscar el encargado.');
+                return false;
+            }
+        } else {
+            showToast('Error al buscar', 'Digite el numero de cédula para buscar el encargado');
         }
     }
+
+    async function deleteManager() {
+        const id = document.getElementById('id').value;
+        let exists = false;
+        if (id) {
+            try {
+                const response = await axios.get(`${url}api/funcionarios`, {
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                });
+                const managers = response.data;
+                managers.forEach(manager => {
+                    if (manager.Cedula == id) {
+                        exists = true;
+                    }
+                });
+                if (exists) {
+                    const response2 = await axios.delete(`${url}api/funcionarios`, id, {
+                        headers: {
+                            'Authorization': `Bearer ${token}`
+                        }
+                    });
+                    showToast('Éxito', 'Encargado eliminado con éxito.');
+                } else {
+                    showToast('Error', 'Encargado no encontrado.');
+                }
+    
+            } catch (error) {
+                console.error('Error al eliminar encargado:', error.response ? error.response.data : error.message);
+                showToast('Error', 'Error al eliminar el encargado.');
+                return false;
+            }
+        } else {
+            showToast('Error', 'Digite el número de cédula para buscar el encargado.');
+        }
+    }
+
+    async function updateManager() {
+        const ced = document.getElementById('id').value;
+        const nombre = document.getElementById('nombre').value;
+        const apellidos = document.getElementById('apellidos').value;
+        const cargo = document.getElementById('cargo').value;
+        const contacto = document.getElementById('contacto').value;
+        const email = document.getElementById('email').value;
+
+        if (ced && nombre && apellidos && cargo && contacto && email) {
+            try {
+                const manager = {
+                    Nombre: nombre,
+                    Apellidos: apellidos,
+                    Cargo: cargo,
+                    Contacto: contacto,
+                    Correo: email,
+                };
+                const response = await axios.put(`${url}api/funcionarios/${ced}`, manager, {
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                });
+                console.log('Respuesta del servidor:', response);
+                showToast('Éxito', 'Encargado actualizado con éxito.');
+                return true;
+            } catch (error) {
+                console.error('Error al actualizar datos:', error.response ? error.response.data : error.message);
+                console.error('Detalles del error:', error.response ? error.response : error);
+                showToast('Error', 'Error al actualizar el encargado.');
+                return false;
+            }
+        } else {
+            showToast('Error', 'Completa la información necesaria');
+        }
+    }
+
 
     btnAdd.addEventListener('click', function () {
-        addManager(); 
+        addManager();
     });
 
-    async function readChofer() {
-        try {
-            const response = await axios.get(`${url}api/chofer`, {
-                headers: {
-                    'Authorization': `Bearer ${token}`
-                }
-            });
-            const choferes = response.data.choferes;
-            let body = '<option selected disabled value="">Seleccione una opción</option>';
-            choferes.forEach(chofer => {
-                body += `<option value="${chofer.idChofer}">${chofer.nombre}</option>`;
-            });
-            document.getElementById('select-chofer').innerHTML = body;
+    btnRead.addEventListener('click', function () {
+        event.preventDefault();
+        readManager();
+    });
 
-        } catch (error) {
-            console.error('No se cargaron los datos', error);
-        }
-    }
+    btnUpdate.addEventListener('click', function () {
+        event.preventDefault();
+        updateManager();
+    });
+
+    btnClear.addEventListener('click', function () {
+        event.preventDefault();
+        document.getElementById('id').value = "";
+        document.getElementById('nombre').value = "";
+        document.getElementById('apellidos').value = "";
+        document.getElementById('cargo').value = "";
+        document.getElementById('contacto').value = "";
+        document.getElementById('email').value = "";
+    });
+
+    btnDelete.addEventListener('click', function () {
+        event.preventDefault();
+        deleteManager();
+    });
 
 })();
