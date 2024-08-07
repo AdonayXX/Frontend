14799bd9d74e08e22d6cd857bb47dbb98586c8ca
AdonayXX <emdiazdu@est.utn.ac.cr>
2024-08-07 10:38:11 -0600

    fix:
    Arreglar error de appointmentsMade.js este no mostraba las citas correctamente.

--- js/appointmentsMade.js
+++ js/appointmentsMade.js
@@ -8,55 +8,67 @@ async function loadCitas() {
         });
         const citas = response.data;
         renderTable(citas);
+        ocultarSpinner();
+
 
         if ($.fn.DataTable.isDataTable('#TableAppointment')) {
             $('#TableAppointment').DataTable().clear().destroy();
         }
-        $('#TableAppointment').DataTable({
-            dom: "<'row'<'col-sm-6'l>" +
-                "<'row'<'col-sm-12't>>" +
-                "<'row'<'col-sm-6'i><'col-sm-6'p>>",
-            ordering: false,
-            searching: true,
-            paging: true,
-            pageLength: 25, 
-            lengthMenu: [ [25, 50, 100, -1], [25, 50, 100, "Todo"] ],
-            language: {
-                url: 'https://cdn.datatables.net/plug-ins/1.10.24/i18n/Spanish.json'
-            },
-            caseInsensitive: true,
-            smart: true
-        });
 
-        $('#searchAppointment').on('keyup', function () {
-            let inputValue = $(this).val().toLowerCase();
-            let selectedState = $('#seleccionar-estado').val().toLowerCase();
-            $('#TableAppointment').DataTable().search(inputValue + ' ' + selectedState).draw();
-        });
+        $.getJSON('https://cdn.datatables.net/plug-ins/1.10.24/i18n/Spanish.json', function (langJson) { //buscar en datatable idioma español y migrarlo a local(pendiente)
+            $('#TableAppointment').DataTable({
+                dom: "<'row'<'col-sm-6'l><'col-sm-6'f>>" +
+                    "<'row'<'col-sm-12't>>" +
+                    "<'row'<'col-sm-6'i><'col-sm-6'p>>",
+                ordering: false,
+                searching: true,
+                paging: true,
+                pageLength: 25,
+                lengthMenu: [ [25, 50, 100, -1], [25, 50, 100, "Todo"] ],
+                language: langJson,
+                columns: [
+                    { data: 'idCita' },
+                    { data: 'nombreCompletoPaciente' },
+                    { data: 'fechaCita' },
+                    { data: 'horaCita' },
+                    { data: 'ubicacionDestino' },
+                    { data: 'tipoSeguro' },
+                    { data: 'estadoCita' },
+                    { data: null, defaultContent: '<button class="btn btn-outline-primary btn-sm" data-bs-toggle="modal" data-bs-target="#AcompananteModal"><i class="bi bi-eye"></i></button>' },
+                    { data: null, defaultContent: '<button class="btn btn-outline-primary btn-sm" data-bs-toggle="modal" data-bs-target="#editarModal"><i class="bi bi-pencil"></i></button>' }
+                ]
+            });
 
-        $('#fechaCita').on('change', function () {
-            let fechaInput = $('#fechaCita').val();
-            $('#TableAppointment').DataTable().column(2).search(fechaInput).draw();
-        });
-
-        $(document).ready(function () {
-            $('#seleccionar-estado').val('Iniciada').trigger('change');
-
-            $('#seleccionar-estado').on('change', function () {
-                let selectedState = $(this).val().toLowerCase();
-                $('#TableAppointment').DataTable().column(6).search(selectedState).draw();
-
-                let tituloCitas = document.getElementById('tituloCitas');
-                tituloCitas.textContent = `Citas ${selectedState.charAt(0).toUpperCase() + selectedState.slice(1)}s`;
-
-                let inputValue = $('#searchAppointment').val().toLowerCase();
+            $('#searchAppointment').on('keyup', function () {
+                let inputValue = $(this).val().toLowerCase();
+                let selectedState = $('#seleccionar-estado').val().toLowerCase();
                 $('#TableAppointment').DataTable().search(inputValue + ' ' + selectedState).draw();
             });
-        });
 
-        $('#TableAppointment').DataTable().search('iniciada').draw();
-        ocultarSpinner();
+            $('#fechaCita').on('change', function () {
+                let fechaInput = $('#fechaCita').val();
+                $('#TableAppointment').DataTable().column(2).search(fechaInput).draw();
+            });
+
+            $(document).ready(function () {
+                $('#seleccionar-estado').val('Iniciada').trigger('change');
+
+                $('#seleccionar-estado').on('change', function () {
+                    let selectedState = $(this).val().toLowerCase();
+                    $('#TableAppointment').DataTable().column(6).search(selectedState).draw();
+
+                    let tituloCitas = document.getElementById('tituloCitas');
+                    tituloCitas.textContent = `Citas ${selectedState.charAt(0).toUpperCase() + selectedState.slice(1)}s`;
+
+                    let inputValue = $('#searchAppointment').val().toLowerCase();
+                    $('#TableAppointment').DataTable().search(inputValue + ' ' + selectedState).draw();
+                });
+            });
+
+            $('#TableAppointment').DataTable().search('iniciada').draw();
+        });
     } catch (error) {
+        console.error('Error al obtener las citas:', error);
         showToast("Error", "Error al obtener las citas.");
     }
 }
@@ -67,7 +79,6 @@ function renderTable(citas) {
     citas.sort((a, b) => {
         const fechaHoraA = new Date(`${a.fechaCita} ${a.horaCita}`);
         const fechaHoraB = new Date(`${b.fechaCita} ${b.horaCita}`);
-
         return fechaHoraB - fechaHoraA;
     });
 
@@ -101,107 +112,6 @@ function renderTable(citas) {
         `;
         tableBody.appendChild(row);
     });
-}
-
-function getAcompanantes(cita) {
-    try {
-        const tableBody = document.getElementById('AcompananteTableBody');
-        tableBody.innerHTML = '';
-
-        const row = document.createElement('tr');
-
-        const infoAdicional = [
-            cita.ubicacionOrigen,
-            cita.transladoCita,
-            cita.camilla,
-            cita.diagnostico,
-            cita.especialidad,
-            cita.prioridad,
-            cita.nombreCompletoAcompanante1 || 'No posee.',
-            cita.nombreCompletoAcompanante2 || 'No posee.'
-        ];
-
-        infoAdicional.forEach(info => {
-            const cell = document.createElement('td');
-            cell.textContent = info;
-            row.appendChild(cell);
-        });
-
-        tableBody.appendChild(row);
-    } catch (error) {
-        showToast("Error", 'Error al obtener los acompañantes.');
-    }
-}
-
-document.querySelector('#searchAppointment').addEventListener('input', function (e) {
-    if (this.value.length > 15) {
-        this.value = this.value.slice(0, 15);
-    }
-});
-
-function editarCita(cita) {
-    document.querySelector('#editarIdCita').value = cita.idCita;
-    document.querySelector('#editarNombrePaciente').value = cita.nombreCompletoPaciente;
-    document.querySelector('#editarFechaCita').value = cita.fechaCita;
-    document.querySelector('#editarHora').value = cita.horaCita;
-    document.querySelector('#seleccionar-destino').value = cita.idUbicacionDestino;
-    document.querySelector('#tipoSeguro').value = cita.tipoSeguro;
-    document.querySelector('#formEditarCita').addEventListener('submit', function (event) {
-        event.preventDefault();
-        updateCita(cita.idCita);
-    });
-}
-
-function getRutas() {
-    const selectDestino = document.getElementById('seleccionar-destino');
-
-    axios.get('https://backend-transporteccss.onrender.com/api/rutas')
-        .then(response => {
-            const destinos = response.data;
-            destinos.forEach(destino => {
-                const option = document.createElement('option');
-                option.value = destino.IdRuta;
-                option.textContent = destino.Descripcion;
-                selectDestino.appendChild(option);
-            });
-        })
-        .catch(() => {
-            showToast("Error", "Error al obtener los destinos.");
-        });
-}
-
-getRutas();
-
-async function updateCita(idCita) {
-    const fechaCita = document.querySelector('#editarFechaCita').value;
-    const horaCita = document.querySelector('#editarHora').value;
-    const idUbicacionDestino = document.querySelector('#seleccionar-destino').value;
-    const tipoSeguro = document.querySelector("#tipoSeguro").value;
-
-    const updatedCitas = {
-        idUbicacionDestino: idUbicacionDestino,
-        fechaCita: fechaCita,
-        horaCita: horaCita,
-        tipoSeguro: tipoSeguro
-    };
-
-    try {
-        const token = localStorage.getItem('token');
-        await axios.put(`https://backend-transporteccss.onrender.com/api/cita/${idCita}`, updatedCitas, {
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        });
-
-        $('#editarModal').modal('hide');
-        setTimeout(function () {
-            loadContent('AppointmentsMade.html', 'mainContent');
-        }, 1400);
-        showToast("¡Éxito!", "Cita actualizada correctamente.");
-    } catch (error) {
-        $('#editarModal').modal('hide');
-        showToast("Error", "Error al actualizar la cita.");
-    }
 }
 
 function ocultarSpinner() {
