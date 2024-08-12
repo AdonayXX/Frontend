0554d22f3936f11502383a2ff049e8ee0133504f
DufoX18 <carodri323@gmail.com>
2024-08-12 12:40:58 -0600

    Correción
    se cambia el lenguaje de todas las tablas ya que estaban con el link y no estaban local.

--- js/correo.js
+++ js/correo.js
@@ -17,10 +17,10 @@ document.getElementById("confirmarCorreo").addEventListener("click", function (e
                 showToast("¡Éxito!", "Correo enviado exitosamente.");
                 setTimeout(function () {
                     window.location.href = "cambioContrasena.html";
-                }, 3000);
+                }, 2500);
             })
             .catch(function () {
-                showToast("Error", "No se pudo enviar el correo.");
+                showToast("Error", "El correo no existe.");
             });
     } else {
         showToast("Atención", "Ingrese un correo válido.");
@@ -28,48 +28,3 @@ document.getElementById("confirmarCorreo").addEventListener("click", function (e
 });
 
 
-// document.getElementById("confirmarCorreo").addEventListener("click", function (event) {
-//     event.preventDefault();
-//     var email = document.getElementById("emailUsuario").value;
-//     var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-
-//     if (emailPattern.test(email)) {
-//         const token = localStorage.getItem('token');
-
-//         axios.get('https://backend-transporteccss.onrender.com/api/usuario', {
-//             headers: {
-//                 'Authorization': `Bearer ${token}`
-//             }
-//         })
-//             .then(function (response) {
-//                 var usuarios = response.data.usuarios;
-//                 var usuario = usuarios.find(u => u.Correo === email);
-
-//                 if (usuario && usuario.Estado !== 'Inactivo') {
-//                     axios.post('https://backend-transporteccss.onrender.com/api/reset-password/forgot-password',
-//                         { email: email },
-//                         {
-//                             headers: {
-//                                 'Authorization': `Bearer ${token}`
-//                             }
-//                         })
-//                         .then(function () {
-//                             showToast("¡Éxito!", "Correo enviado exitosamente.");
-//                             setTimeout(function () {
-//                                 window.location.href = "cambioContrasena.html";
-//                             }, 3000);
-//                         })
-//                         .catch(function () {
-//                             showToast("Error", "No se pudo enviar el correo.");
-//                         });
-//                 } else {
-//                     showToast("Atención", "El correo no existe o el usuario está inactivo.");
-//                 }
-//             })
-//             .catch(function () {
-//                 showToast("Error", "Hubo un problema al verificar el usuario.");
-//             });
-//     } else {
-//         showToast("Atención", "Ingrese un correo válido.");
-//     }
-// });
